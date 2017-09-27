08b304d2761c9d757f4623b2acb6819dbc636364
Michael Shinners <mishinners@icloud.com>
2017-09-27 10:22:45 -0700

    completed 4/8 TODOs. Passing off to Mark

--- mike-mark/public/scripts/models/repo.js
+++ mike-mark/public/scripts/models/repo.js
@@ -7,11 +7,13 @@ var app = app || {};
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
     //       Remember that the callback function we'll want to call relies on repos.all
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
-
+    $.get('/github/users/repos')
+      .then(response => repos.all = response)
+      .then(callback)
   };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
