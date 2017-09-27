86ca4b1ae31c9ede87fa816c9a6e38fa5cd862ef
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-09-27 13:36:54 -0700

    fixed about controller app method call to actually call the render method from repoView

--- jacob-george/starter-code/public/scripts/controllers/aboutController.js
+++ jacob-george/starter-code/public/scripts/controllers/aboutController.js
@@ -9,7 +9,7 @@ var app = app || {};
 
     // DONE: Call a function to load all the data.
     // Pass a view function as a callback, so the view will render after the data is loaded.
-    app.repoView(app.articleView.initIndexPage)
+    app.repos.requestRepos(app.repoView.index)
   };
 
   module.aboutController = aboutController;
