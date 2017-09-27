758e19f0c812ca6f0ecfc38f0670b9b290e52a60
Michael Shinners <mishinners@icloud.com>
2017-09-27 10:03:19 -0700

    completed 2 TODOs

--- mike-mark/server.js
+++ mike-mark/server.js
@@ -3,12 +3,14 @@
 const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
+const expressProxy = require('express-request-proxy');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = ''; // TODO: Don't forget to set your own conString
+const conString = 'postgres://localhost5432/kilovolt'; // DONE: Don't forget to set your own conString
 const client = new pg.Client(conString);
+
 client.connect();
 client.on('error', err => console.error(err));
 
@@ -28,7 +30,19 @@ app.get('/articles', (request, response) => {
   .catch(console.error);
 });
 
-// TODO: Whenever a "get" request is sent to a route starting with "/github/", use the remainder of that url to access github, with your token as the Authorization header. You should *only* make direct requests to Github from server.js, not from the front-end. What you send back will be a collection of repositories, as an array of objects.
+// DONE: Whenever a "get" request is sent to a route starting with "/github/", use the remainder of that url to access github, with your token as the Authorization header. You should *only* make direct requests to Github from server.js, not from the front-end. What you send back will be a collection of repositories, as an array of objects.
+
+app.get('/github/*', proxyGithub);
+
+function proxyGithub(req, res) {
+  (expressProxy({
+    url: `https://api.github.com/${req.params[0]}`,
+    headers: {
+      Authorization: `token
+      ${process.env.githubToken}`
+    }
+  }))(req, res);
+}
 
 app.post('/articles', function(request, response) {
   client.query(
