9cd3959d685a2be4ebc134d20c95917a617e7f94
George Embry <georgetembry@yahoo.com>
2017-09-27 11:08:30 -0700

    completed TODO's in index.html, and server.js Todo. created get request to send a route to github with token as Authorization header.

--- jacob-george/starter-code/server.js
+++ jacob-george/starter-code/server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const requestProxy = require('express-request-proxy');
 const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
@@ -28,7 +29,13 @@ app.get('/articles', (request, response) => {
   .catch(console.error);
 });
 
-// TODO: Whenever a "get" request is sent to a route starting with "/github/", use the remainder of that url to access github, with your token as the Authorization header. You should *only* make direct requests to Github from server.js, not from the front-end. What you send back will be a collection of repositories, as an array of objects.
+// DONE: Whenever a "get" request is sent to a route starting with "/github/", use the remainder of that url to access github, with your token as the Authorization header. You should *only* make direct requests to Github from server.js, not from the front-end. What you send back will be a collection of repositories, as an array of objects.
+app.get('/github/*', requestProxy({
+  url: 'https://api.github.com/*',
+  headers: {
+    Authorization: `token ${process.env.GITHUBTOKEN}`
+  }
+}));
 
 app.post('/articles', function(request, response) {
   client.query(
